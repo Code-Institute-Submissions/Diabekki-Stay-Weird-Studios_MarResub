cbe5a24ecac6968cca4401b974d946f61fa5cc33
Diabekki <bekkitubridy@gmail.com>
2021-12-20 17:54:06 +0000

    Added merchandise templates

--- merchandise/urls.py
+++ merchandise/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='homepage')
+    path('', views.all_merchandise, name='merchandise')
 ]
--- merchandise/views.py
+++ merchandise/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Merch
 
 # Create your views here.
+
+
+def all_merchandise(request):
+    """ A view to show merchandise, including sorting and searching """
+
+    merchandise = Merch.objects.all()
+
+    context = {
+        'merchandise': merchandise,
+    }
+
+    return render(request, 'merchandise/merchandise.html', context)
