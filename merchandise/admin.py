590610b09d4644633041bb486061dd8d533d31d9
Diabekki <bekkitubridy@gmail.com>
2021-12-20 17:40:02 +0000

    Added models for merch and categories to admin

--- merchandise/admin.py
+++ merchandise/admin.py
@@ -2,5 +2,26 @@ from django.contrib import admin
 from .models import Merch, Category
 
 # Register your models here.
-admin.site.register(Merch)
-admin.site.register(Category)
+
+
+class MerchAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Merch, MerchAdmin)
+admin.site.register(Category, CategoryAdmin)
