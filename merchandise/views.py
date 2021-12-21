8d973d5611e0b6a5e4f8eb74398f26ebb6c55f0c
Diabekki <bekkitubridy@gmail.com>
2021-12-21 19:51:44 +0000

    Added search funtion to merchanise page

--- merchandise/views.py
+++ merchandise/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Merch
 
 # Create your views here.
@@ -8,9 +10,21 @@ def all_merchandise(request):
     """ A view to show merchandise, including sorting and searching """
 
     merchandise = Merch.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter criteria to search for")
+                return redirect(reverse('merchandise'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            merchandise = merchandise.filter(queries)
 
     context = {
         'merchandise': merchandise,
+        'search_term': query
     }
 
     return render(request, 'merchandise/merchandise.html', context)
