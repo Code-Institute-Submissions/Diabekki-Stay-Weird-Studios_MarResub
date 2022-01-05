d58a8f63a7025c7e336c1c7e36f9307085c9b5af
Diabekki <bekkitubridy@gmail.com>
2022-01-05 16:32:14 +0000

    Added shopping cart quantity selection

--- cart/urls.py
+++ cart/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_cart, name='view_cart')
+    path('', views.view_cart, name='view_cart'),
+    path('add/<merch_id>/', views.shopping_cart_quantity, name='shopping_cart_quantity'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 
@@ -7,3 +8,21 @@ def view_cart(request):
     """ A view for the shopping cart page """
 
     return render(request, 'cart/cart.html')
+
+
+def shopping_cart_quantity(request, merch_id):
+    """ Number of items added to shopping cart """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if merch_id in list(cart.keys()):
+        cart[merch_id] += quantity
+    else: 
+        cart[merch_id] = quantity
+
+        request.session['cart'] = cart
+        print(request.session['cart'])
+        return redirect(redirect_url)
+
