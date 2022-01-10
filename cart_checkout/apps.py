139d97822f06a2019a35e0d8944cf767cf35be4d
Diabekki <bekkitubridy@gmail.com>
2022-01-10 13:39:48 +0000

    Added signals, admin and forms

--- cart_checkout/apps.py
+++ cart_checkout/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CartCheckoutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cart_checkout'
+
+    def ready(self):
+        import cart_checkout.signals
---
+++ cart_checkout/signals.py
@@ -0,0 +1,20 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import PurchaseLineItem
+
+
+@receiver(post_save, sender=PurchaseLineItem())
+def update_save(sender, instance, created, **kwargs):
+    """
+    Update order total on lineitem update/create
+    """
+    instance.purchase.update_total()
+
+
+@receiver(post_delete, sender=PurchaseLineItem())
+def update_delete(sender, instance, **kwargs):
+    """
+    Update order total on lineitem delete
+    """
+    instance.purchase.update_total()
