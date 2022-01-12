3b26428452e5db170b4ece56f8d16a9923359ba7
Diabekki <bekkitubridy@gmail.com>
2022-01-12 16:41:55 +0000

    Added class to webhooks

--- cart_checkout/urls.py
+++ cart_checkout/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
+
 
 urlpatterns = [
     path('', views.cart_checkout, name='cart_checkout'),
     path('cart_checkout_success/<purchase_number>', views.cart_checkout_success, name='cart_checkout_success'),
+    path('wh/', webhook, name='webhook'),
 ]
