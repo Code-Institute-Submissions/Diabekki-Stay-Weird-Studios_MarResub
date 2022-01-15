f992cc61dff32020d2bfc52e7a366eab5069ea32
Diabekki <bekkitubridy@gmail.com>
2022-01-15 18:04:26 +0000

    Removed secret key and set debug

--- stay_weird_studios/settings.py
+++ stay_weird_studios/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)hwi$oslypdo7x0g#2r^=a8=g@=akp%v%oix%lfj$5wbm^_27#'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['stay-weird-studios.herokuapp.com', 'localhost']
 
