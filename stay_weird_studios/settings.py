b99657f6a59767d1777e263092519785095cdd63
Diabekki <bekkitubridy@gmail.com>
2022-01-21 11:44:54 +0000

    Fixing 500 error issue with sending real emails - account/signup error 3

--- stay_weird_studios/settings.py
+++ stay_weird_studios/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'yoursecretkey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
+# 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['stay-weird-studios.herokuapp.com', 'localhost']
 
@@ -213,7 +214,7 @@ else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST = 'bekkitubridy@gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMIAL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
